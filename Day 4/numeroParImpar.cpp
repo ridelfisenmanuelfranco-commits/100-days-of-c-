96f282fd6cb189181f5dd4a9bdb9e94b609a9c52
ridelfis <ridelfisenmanuelfranco@gmail.com>
2026-07-28 18:16:08 -0400

    Creacion de un programa para identificas si un numero digitado por el usuario es par o impar

---
+++ Day 4/numeroParImpar.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+	int numero;
+
+	std::cout << "Ingresa un numero: "; std::cin >> numero;
+
+	if (numero % 2 == 0) {
+		std::cout << "El numero: " << numero << ", es par." << std::endl;
+	}
+	else {
+		std::cout << "El numero: " << numero << ", es impar." << std::endl;
+	}
+	return 0;
+}
